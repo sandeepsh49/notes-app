ab0e67657de74e1705ea2e124371a4dacd6ced44
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-04-27 00:14:04 +0530

    Change import

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getNote, createNote, updateNote, deleteNote } = require('../controllers/noteControllers')
+const { getNote, createNote, updateNote, deleteNote } = require('../controllers/noteController')
 
 router.route('/').get(getNote).post(createNote)
 
