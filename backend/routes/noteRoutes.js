a93517273a5d4a2eb4fbf39fffac3cebe6dbb4e4
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-04-29 00:54:08 +0530

    Update routes

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getNotes, createNote, updateNote, deleteNote } = require('../controllers/noteController')
+const { getNotes, setNote, updateNote, deleteNote } = require('../controllers/noteController')
 const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getNotes).post(protect, createNote)
+router.route('/').get(protect, getNotes).post(protect, setNote)
 
 router.route('/:id').put(protect, updateNote).delete(protect, deleteNote)
 
