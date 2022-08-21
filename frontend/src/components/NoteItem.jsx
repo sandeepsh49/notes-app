47038f04d3dbcc4d17b9cf7f4b35de7434619b22
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-08-21 16:36:11 +0530

    Minor stylesheet changes

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,18 +1,18 @@
-import { useDispatch } from 'react-redux'
-import { deleteNote } from '../features/notes/noteSlice'
+import { useDispatch } from 'react-redux';
+import { deleteNote } from '../features/notes/noteSlice';
 
 function NoteItem({ note }) {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-  return (
-    <div className='note'>
-      <div>{new Date(note.createdAt).toLocaleString('en-US')}</div>
-      <h2>{note.text}</h2>
-      <button onClick={() => dispatch(deleteNote(note._id))} className='close'>
-        X
-      </button>
-    </div>
-  )
+	return (
+		<div className="note">
+			<div className="note-date">{new Date(note.createdAt).toLocaleString('en-US')}</div>
+			<h2>{note.text}</h2>
+			<button onClick={() => dispatch(deleteNote(note._id))} className="close">
+				&#10006;
+			</button>
+		</div>
+	);
 }
 
-export default NoteItem
+export default NoteItem;
