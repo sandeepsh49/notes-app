67ec4956b58275aafa01acbdb0eaeea11134c7c1
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-08-28 22:19:24 +0530

    Change Edit position

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -6,12 +6,12 @@ function NoteItem({ note, handleEdit }) {
 
 	return (
 		<div className="note">
+			<button onClick={e => handleEdit(note._id, note.text)}>Edit</button>
 			<div>{new Date(note.createdAt).toLocaleString('en-US')}</div>
 			<h2>{note.text}</h2>
 			<button onClick={() => dispatch(deleteNote(note._id))} className="close">
 				&#10006;
 			</button>
-			<button onClick={e => handleEdit(note._id, note.text)}>Edit</button>
 		</div>
 	);
 }
