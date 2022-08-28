628da4b3d750a12692c9aa15449ca5188f453708
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-08-28 22:52:42 +0530

    Add edit button styles

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -6,9 +6,11 @@ function NoteItem({ note, handleEdit }) {
 
 	return (
 		<div className="note">
-			<button onClick={e => handleEdit(note._id, note.text)}>Edit</button>
-			<div>{new Date(note.createdAt).toLocaleString('en-US')}</div>
+			<button onClick={e => handleEdit(note._id, note.text)} className="edit">
+				Edit
+			</button>
 			<h2>{note.text}</h2>
+			<div>{new Date(note.createdAt).toLocaleString('en-US')}</div>
 			<button onClick={() => dispatch(deleteNote(note._id))} className="close">
 				&#10006;
 			</button>
